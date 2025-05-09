bfef129fbb6005932e9183be0883cf118f1b0701
HilmiRF <hilmirf23@gmail.com>
2025-05-09 10:38:47 +0700

    fix: fix navbar link

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -9,10 +9,10 @@ interface NavbarProps {
 
 const Navbar: FC<NavbarProps> = ({ isHome }) => {
 	return (
-		<Link href={"/"}>
-			<div className="navbar bg-base-100 rounded-box shadow-lg mb-4 md:mb-6 max-w-[40rem] mx-auto">
-				<div className={`navbar-start ${isHome ? "" : "w-1/4"}`}>
-					<div className="btn btn-square btn-ghost">
+		<div className="navbar bg-base-100 rounded-box shadow-lg mb-4 md:mb-6 max-w-[40rem] mx-auto">
+			<div className={`navbar-start ${isHome ? "" : "w-1/4"}`}>
+				<button className="btn btn-square btn-ghost">
+					<Link href={"/"}>
 						<div className="relative w-full aspect-square m-1">
 							<span className="box-sizing:border-box;display:block;overflow:hidden;width:initial;height:initial;background:none;opacity:1;border:0;margin:0;padding:0;position:absolute;top:0;left:0;bottom:0;right:0">
 								<Image
@@ -23,32 +23,32 @@ const Navbar: FC<NavbarProps> = ({ isHome }) => {
 								/>
 							</span>
 						</div>
-					</div>
-				</div>
-				<div
-					className={`navbar-center ${
-						isHome ? "" : "w-1/2 justify-center"
-					}`}
-				>
-					{isHome ? (
-						<div className="text-center text-xl">Mood2Anime</div>
-					) : (
-						<div className="text-center tracking-tight">
-							Feeling 😕 Gloomy
-						</div>
-					)}
-				</div>
-				<div className={`navbar-end ${isHome ? "" : "w-1/4"}`}>
-					{isHome ? (
-						""
-					) : (
-						<button className="btn btn-primary btn-square md:btn-block">
-							Edit Mood
-						</button>
-					)}
-				</div>
+					</Link>
+				</button>
 			</div>
-		</Link>
+			<div
+				className={`navbar-center ${
+					isHome ? "" : "w-1/2 justify-center"
+				}`}
+			>
+				{isHome ? (
+					<div className="text-center text-xl">Mood2Anime</div>
+				) : (
+					<div className="text-center tracking-tight">
+						Feeling 😕 Gloomy
+					</div>
+				)}
+			</div>
+			<div className={`navbar-end ${isHome ? "" : "w-1/4"}`}>
+				{isHome ? (
+					""
+				) : (
+					<button className="btn btn-primary btn-square md:btn-block">
+						Edit Mood
+					</button>
+				)}
+			</div>
+		</div>
 	);
 };
 
